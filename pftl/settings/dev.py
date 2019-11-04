873f2378cbd3517bc617d75391f7ecc4f6d99998
Aquiles Carattino <aquiles@uetke.com>
2019-11-04 15:51:16 +0100

    Initial commit with Wagtail site

---
+++ pftl/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '6vnsnsy!ppyy7+qa+-iaquw4)hhl72g7#33(h1y$g92zj@&4g='
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
