dc284c8c04b4c8d7c1d59671c652e4547956bb38
Aquiles Carattino <aquiles@uetke.com>
2019-12-28 18:49:20 +0100

    Started working on courses page

--- pftl/urls.py
+++ pftl/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
@@ -28,7 +29,7 @@ urlpatterns = [
 
     path('free_chapter/', RequestFreeChapter.as_view(), name='request-free-chapter'),
     path('confirm/<str:secret_code>', ConfirmFreeChapter.as_view(), name='confirm-free-chapter'),
-
+    path('courses', TemplateView.as_view(template_name='courses/index.html'), name='courses'),
     url('^sitemap\.xml$', sitemap),
     url(r'feed\.rss$', BlogsFeed(), name='feed'),
 
---
+++ static/courses/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$(function () {
+    page.config({
+        googleApiKey: '',
+        googleAnalyticsId: '',
+        reCaptchaSiteKey: '',
+        reCaptchaLanguage: '',
+        disableAOSonMobile: true,
+        smoothScroll: true,
+    });
+});
+
