550408db299e292215ffe0f98bbe9c174952d12b
Aquiles Carattino <aqui.carattino@gmail.com>
2020-04-26 08:32:16 +0200

    First attempt to move PFTL to containers

--- free_chapter/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from free_chapter.models import People, FreeChapter
-
-admin.site.register((
-    People,
-    FreeChapter))
--- free_chapter/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FreeChapterConfig(AppConfig):
-    name = 'free_chapter'
