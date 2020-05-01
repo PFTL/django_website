023f79d04bffd6f2b5ef89fee2b5ce7f55a050de
Aquiles Carattino <aqui.carattino@gmail.com>
2020-05-01 09:21:52 +0200

    Added headers and 400 response

--- git_book/views.py
+++ git_book/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
@@ -14,4 +14,9 @@ class SubscribeGitView(View):
             if Subscription.objects.filter(email=email).exists():
                 return JsonResponse({'message': 'Already signed up'})
             Subscription.objects.create(email=email)
-            return JsonResponse({'message': 'Thank you for signing up!'})
+            response =  JsonResponse({'message': 'Thank you for signing up!'})
+            response['Access-Control-Allow-Origin'] = '*'
+            return response
+
+        return HttpResponse(status=400)
+
