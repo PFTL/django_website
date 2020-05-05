d79877cf1a2499a937d9e3f80385b367640f1875
Aquiles Carattino <aqui.carattino@gmail.com>
2020-05-05 08:01:54 +0200

    Adding children to blog

--- pftl/blog/management/commands/import_json_articles.py
+++ pftl/blog/management/commands/import_json_articles.py
@@ -3,14 +3,15 @@ import os
 
 from django.core.management import BaseCommand
 
-from pftl.blog.models import BlogPage
+from pftl.blog.models import BlogPage, BlogIndexPage
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         file_path = '/webapps/pftl/articles_archive'
+        index_page = BlogIndexPage.objects.get_or_create(title='Blog')
         for file in os.listdir(file_path):
             with open(os.path.join(file_path, file), 'r') as f:
                 data = json.load(f)
                 page = BlogPage(**data)
-                page.save()
+                index_page.add_child(page)
