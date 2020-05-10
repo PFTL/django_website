6d496bd9bf280518582fd9448746d39112a5ef94
Aquiles Carattino <aqui.carattino@gmail.com>
2020-05-10 14:55:55 +0200

    Sitemap and robots

--- config/settings/base.py
+++ config/settings/base.py
@@ -56,6 +56,7 @@ DJANGO_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
+    'django.contrib.sitemaps',
     "django.contrib.sites",
     "django.contrib.messages",
     "django.contrib.staticfiles",
