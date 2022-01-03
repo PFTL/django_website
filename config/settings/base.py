f0caf20e23932dc28482e1e1adeee689fca1a0ea
Aquiles Carattino <aqui.carattino@gmail.com>
2022-01-03 15:37:10 +0100

    Removed email middleware for broken links

--- config/settings/base.py
+++ config/settings/base.py
@@ -154,7 +154,6 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
